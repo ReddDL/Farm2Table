9eb6172bd91603222638adaa0988d6a1ec8d3eaf
ReddDL <redeleon.dev@gmail.com>
2024-05-08 11:02:37 +0800

    chore: added color variables

--- finalprojectapp/tailwind.config.js
+++ finalprojectapp/tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      'midnight-green': "#1B4650",
+      'tea-green': "#DCFEBC",
+      'eggshell': "#EDEBD6",
+      'oxford-blue': "#141427",
+      'gunmetal': "#0F2D2D",
+      'periwinkle': "#C2B7ED",
+      'orchid-pink': '#F4B8D1',
+      'alabaster': "#F4F3E8",
+      'space-cadet': "#1A1A31"
+    },
     extend: {},
   },
   plugins: [],
