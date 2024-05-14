5d2fad72de13c315009747ccfec40cd211959d3e
ReddDL <redeleon.dev@gmail.com>
2024-05-14 15:16:37 +0800

    feat: add hover animation

--- finalprojectapp/src/pages/Header.js
+++ finalprojectapp/src/pages/Header.js
@@ -1,14 +1,21 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
+import './Landing.css';
 
 const Header = () => {
   return (
     <div className='flex justify-center h-fit shadow-md'>
         <div className=' bg-midnight-green h-16 absolute top-5 rounded-xl z-10 w-full min-w-xs max-w-7xl flex justify-between items-center shadow-xl px-5 '>
-            <div className='flex flex-1 gap-4 justify-start'>
-                <NavLink className='text-tea-green' to='/products'> Products </NavLink>
-                <NavLink className='text-tea-green' to='/products'> Orders </NavLink>
-            </div>
+            {/* <div className='flex flex-1 gap-4 justify-start'> */}
+                <ul className='flex flex-1 gap-4 justify-start'>
+                <li>
+                    <NavLink className='text-tea-green text-lg' to='/products'> Products </NavLink>
+                </li>
+                <li>
+                    <NavLink className='text-tea-green text-lg' to='/products'> Orders </NavLink>
+                </li>
+                </ul>
+            {/* </div> */}
             <div className=' flex flex-1 justify-center'>
                 <NavLink className='text-tea-green volkhov-regular text-2xl ' to='/'> Farm2Table </NavLink>
             </div>
