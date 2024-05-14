102fd04290f9466dbf69f0fe63dfc01da22a9cd6
ReddDL <redeleon.dev@gmail.com>
2024-05-14 22:59:41 +0800

    fix: returned default render

--- finalprojectapp/src/index.js
+++ finalprojectapp/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Signup from './pages/Signup';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Signup />
+    <App />
   </React.StrictMode>
 );
 
