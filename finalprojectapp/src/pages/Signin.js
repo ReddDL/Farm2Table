7218ae8887142355458d01dae03d030af6193b57
ReddDL <redeleon.dev@gmail.com>
2024-05-19 14:52:43 +0800

    refactor: changed layout

--- finalprojectapp/src/pages/Signin.js
+++ finalprojectapp/src/pages/Signin.js
@@ -49,46 +49,36 @@ function Signin() {
     
 
     return (
-      <div className ="bg-eggshell w-screen h-screen text-gunmetal poppins-light border-2 border-solid border-black">
-            {/* <div className='h-screen w-full relative flex justify-center'> */}
-                {/* <img src={GreenTopo} autoPlay loop muted className='h-full w-full object-cover'/> */}
-            {/* </div> */}
+      <div className ="bg-periwinkle w-screen h-screen text-gunmetal poppins-light p-3">
             <section className="z-10 relative top-1/2 translate-y-[-50%] mx-auto p-8 rounded-2xl max-w-lg bg-alabaster">
-                <h1 className="text-4xl mb-3 poppins-regular">Login</h1>
-                {/* <h1 className = "text-2xl mb-8 poppins-light">Are you ready to support our farmers?</h1> */}
-                <form onSubmit={handleLogin} className='w-full'>
-                    {/* Email */}
-                    <input 
-                        placeholder="Email" 
-                        className="rounded-md h-12 w-full bg-alabaster border-2 border-gunmetal mb-4 p-2" 
-                        type="text" 
-                        id="email"
-                        onChange={(e) => setEmail(e.target.value)}/>
-                    {/* Password */}
-                    {/* <label className="mt-4">Password:</label> */}
-                    <div>
-                        <input type={showPassword ? 'text' : 'password'}
-                                placeholder="Password" 
-                                className="rounded-md h-12 bg-alabaster border-2 border-gunmetal w-full p-2" 
-                                id="password"
-                                onChange={(e) => setPassword(e.target.value)}
-                                />
-                        <button className="float-end mt-4 mr-3" onClick={togglePasswordVisibility} type="submit">
-                            {/* {showPassword ? <FaEyeSlash /> : <FaEye />} */}
-                        </button>
-                    </div>
-                    <button className="bg-gunmetal text-alabaster w-full mt-3 h-14 rounded-xl ">Sign in</button>
-                </form>
-                
+            <h1 className="text-4xl mb-3 lato-bold">Login</h1>
+            <form onSubmit={handleLogin} className='w-full'>
+                {/* Email */}
+                <input 
+                    placeholder="Email" 
+                    className="rounded-lg h-12 w-full bg-alabaster border border-gray-400 mb-4 p-2" 
+                    type="text" 
+                    id="email"
+                    onChange={(e) => setEmail(e.target.value)}/>
+                {/* Password */}
                 <div>
-                    <label>Don't have an account yet? <a href="/sign-up" className='underline text-periwinkle poppins-medium'>Sign up</a></label>
+                    <input type={showPassword ? 'text' : 'password'}
+                            placeholder="Password" 
+                            className="rounded-lg h-12 bg-alabaster border border-gray-400 w-full p-2" 
+                            id="password"
+                            onChange={(e) => setPassword(e.target.value)}
+                            />
+                    <button className="float-end mt-4 mr-3" onClick={togglePasswordVisibility} type="submit">
+                        {/* {showPassword ? <FaEyeSlash /> : <FaEye />} */}
+                    </button>
                 </div>
-                
-        </section>
-        {/* <div className = "w-1/2 h-screen">
-            <img className = "object-cover w-full h-full" src = {require('../images/farmer.jpg')}/>
-        </div> */}
+                <button className="bg-gunmetal text-alabaster w-full mt-3 h-10 rounded-xl lato-regular text-lg">Sign in</button>
+            </form>
             
+            <div>
+                <label className='text-sm'>Don't have an account yet? <a href="/sign-up" className='underline text-periwinkle poppins-medium'>Sign up</a></label>
+            </div>
+        </section>
       </div>
     );
   }
