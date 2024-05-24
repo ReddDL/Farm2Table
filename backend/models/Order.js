2262f16a17e15ec7be88c5cf86d3e8bf742e40f6
rdfez <rdfernandez3@up.edu.ph>
2024-05-25 07:43:06 +0800

    chore: Reverted order schema

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -2,10 +2,8 @@ import mongoose from 'mongoose';
 
 const orderSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    items: [{
-      productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-      quantity: { type: Number, required: true },
-    }],
+    productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+    quantity: { type: Number, required: true },
     status: { type: Number, enum: [0, 1, 2], default: 0 },
     email: { type: String, required: true },
     dateOrdered: { type: Date, default: Date.now }
