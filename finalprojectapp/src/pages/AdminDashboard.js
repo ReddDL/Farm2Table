c9e9fdc4108641dc7608f9934c9b1c7fb5fc0b1a
rdfez <rdfernandez3@up.edu.ph>
2024-05-25 09:26:35 +0800

    feat: Added fetching of sales and orders

--- finalprojectapp/src/pages/AdminDashboard.js
+++ finalprojectapp/src/pages/AdminDashboard.js
@@ -1,20 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AdminOrderCardDash from '../components/admin/AdminOrderCardDash';
+import axios from 'axios';
 
 const AdminDashboard = () => {
     const [sales, setSales] = useState(0);
     const [orders, setOrders] = useState([]);
-    const [interval, setInterval] = useState('Annually');
+    const [interval, setInterval] = useState('Annual');
+    const [loading, setLoading] = useState(false);
 
     const activeLink = 'relative tracking-[1px] mb-4 text-periwinkle after:content-[""] after:bg-periwinkle after:h-[3px] after:w-[100%] after:left-0 after:-bottom-[5px] after:rounded-xl after:absolute';
     const inactiveLink = 'relative tracking-[1px] mb-4 hover:text-periwinkle after:content-[""] after:bg-periwinkle after:h-[3px] after:w-[0%] after:left-0 after:-bottom-[5px] after:rounded-xl after:absolute after:duration-300 hover:after:w-[100%]';
 
     const handleIntervalChange = (newInterval) => {
         setInterval(newInterval);
+        // fetch and set sales
+        fetchSales(newInterval.toLowerCase());
         // You might want to update sales data here based on the newInterval
         // Example: setSales(fetchSalesData(newInterval));
     };
-
+    
     const dummyOrders = [
         {
           productId: 123345345,
@@ -38,6 +42,34 @@ const AdminDashboard = () => {
           dateOrdered: '07/20/2003'
         },
       ]
+    
+    // fetch all customer orders and sales once component is mounted
+    useEffect(() => {
+        async function fetchAllOrders () {
+            try{
+                const res = await axios.get("http://localhost:3000/api/orders/getAllOrders");
+                // update state variable
+                setOrders(res.data);
+            } catch (error) {
+                console.log("Error fetching all orders")
+            }
+        }
+        fetchAllOrders();
+        fetchSales(interval.toLowerCase());
+    }, [])
+
+    // fetch all sales
+    async function fetchSales (interval) {
+        setLoading(true);
+        try{
+            const res = await axios.get(`http://localhost:3000/api/sales/interval?interval=${interval}`);
+            // update state variable
+            setSales(res.data.totalSalesAmount)
+        } catch (error) {
+            console.log("Error fetching sales")
+        }
+        setLoading(false);
+    }
 
     return (
         <div className='bg-eggshell flex flex-col px-8 lg:px-32 md:px-24 sm:px-10 pt-32 min-h-screen'>
@@ -45,8 +77,8 @@ const AdminDashboard = () => {
             <ul className='poppins-regular flex gap-5 mb-4 text-lg'>
                 <li>
                     <button 
-                        onClick={() => handleIntervalChange('Weekly')}
-                        className={interval === 'Weekly' ? activeLink : inactiveLink}
+                      onClick={() => handleIntervalChange('Weekly')}
+                      className={interval === 'Weekly' ? activeLink : inactiveLink}
                     >
                         Weekly
                     </button>
@@ -61,21 +93,29 @@ const AdminDashboard = () => {
                 </li>
                 <li>
                     <button 
-                    onClick={() => handleIntervalChange('Annually')} 
-                    className={interval === 'Annually' ? activeLink : inactiveLink}>
-                        Annually
+                    onClick={() => handleIntervalChange('Annual')} 
+                    className={interval === 'Annual' ? activeLink : inactiveLink}>
+                        Annual
                     </button>
                 </li>
             </ul>
             <div className='bg-alabaster h-52 rounded-xl flex items-center pl-20'>
                 <div className='flex flex-col '>
-                    <h2 className='text-5xl poppins-regular'>{sales}</h2>
-                    <p>{interval} Sales</p>
+                  {
+                      loading ? (
+                          <h2 className='text-5xl poppins-regular'>Loading...</h2>
+                      ) : (
+                          <>
+                            <h2 className='text-5xl poppins-regular'>{sales}</h2>
+                            <p>{interval} Sales</p>
+                          </>
+                      )
+                  }
                 </div>
             </div>
             <div className='bg-alabaster p-5 mb-5 rounded-xl mt-7 flex flex-row sm:flex-col md:flex-row sm:items-center flex-wrap justify-start gap-10'>
-            {dummyOrders.map((order) => (
-                <AdminOrderCardDash key={order.productId} order={order} />
+            {orders.map((order) => (
+                <AdminOrderCardDash key={order._id} order={order} />
             ))}
             </div>
         </div>
